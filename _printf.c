06ccc244ad235936b4ccae85c51fa1a4e7f0fb9e
Tokaallah <tokallah.92@gmail.com>
2023-03-25 06:08:27 -0700

    modify main header file

--- _printf.c
+++ _printf.c
@@ -2,38 +2,46 @@
 #include <stdarg.h>
 #include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int count = 0;
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                char c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's') {
-                char *str = va_arg(args, char *);
-                while (*str != '\0') {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*format == 'd' || *format == 'i') {
-                int num = va_arg(args, int);
-                printf("%d", num);
-                count++;
-            }
-            else if (*format == '%') {
-                putchar('%');
-                count++;
-            }
-        }
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				printf("%d", num);
+				count++;
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
         else {
             putchar(*format);
             count++;
